22a5d7fd5dedd0353070b539d01173edc216480a
Vinicius Flores <viniciusflores379@gmail.com>
2024-07-28 17:54:21 -0300

    :rocket: Complete Task

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-function transformBinaryToDecimal(num){
+function transformBinaryToDecimal(num) {
   const toArray = num.split('')
   let partialValue = 0
 
-  for(let i = 0; i < toArray.length; i++){
+  for (let i = 0; i < toArray.length; i++) {
     const value = parseInt(toArray[i])
 
     partialValue += value * Math.pow(2, toArray.length - i - 1)
@@ -10,22 +10,24 @@ function transformBinaryToDecimal(num){
   return partialValue
 }
 
-function getDataFromInput(){
+function getDataFromInput() {
   const inputValue = document.getElementById('input').value
   const isValidValueFromInput = new RegExp('^[0-1]+$');
 
-  if(inputValue.length === 0){
+  if (inputValue.length === 0) {
     document.getElementById('result').innerHTML = "";
     return
   }
 
   const validRegex = inputValue.match(isValidValueFromInput)
-  if(validRegex == null && inputValue.length > 0){
+  if (validRegex == null && inputValue.length > 0) {
     console.error('This is error: input data invalid')
     document.getElementById('result').innerHTML = 'Invalid input data: Please type 0 or 1 only';
+    document.getElementById('result').style.color = 'red'
     return
   }
 
   const result = transformBinaryToDecimal(inputValue)
+  document.getElementById('result').style.color = 'white'
   document.getElementById('result').innerHTML = "The decimal value is: " + result;
 }
